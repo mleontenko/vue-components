8b00be7d646c695cdca31a26ecdbe523c1bb64c6
Marin Leontenko <marin.leontenko@gmail.com>
2021-11-02 10:40:51 +0100

    validating props

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -6,18 +6,34 @@
         <ul v-if="detailsAreVisible">
             <li><strong>Phone:</strong> {{ phoneNumber }}</li>
             <li><strong>Email:</strong> {{ emailAddress }}</li>
-        </ul>isFavorite
+        </ul>
     </li>
 </template>
 
 <script>
 export default {
-    props: [
-        'name',
-        'phoneNumber',
-        'emailAddress',
-        'isFavorite'
-    ],
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
+        emailAddress: {
+            type: String,
+            required: true,
+        },
+        isFavorite: {
+            type: String,
+            required: false,
+            default: '0',
+            validator: function(value) {
+                return value === '1' || value === '0';
+            }
+        }
+    },
     data() {
         return {
             detailsAreVisible: false,
