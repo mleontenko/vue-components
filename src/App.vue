17ca6f1e3969c3d471097a183a35a27d89df8039
Marin Leontenko <marin.leontenko@gmail.com>
2021-11-02 13:59:22 +0100

    new friend component, connect new component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <section>
         <header><h1>My Friends</h1></header>
+        <new-friend @add-contact="addContact"></new-friend>
         <ul>
             <friend-contact
                 v-for="friend in friends"
@@ -44,6 +45,15 @@ export default {
                 friend => friend.id === friendId
             );
             identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+        },
+        addContact(name, phone, email) {
+            const newFriendContact = {
+                id: new Date().toISOString(),
+                name: name,
+                phone: phone,
+                email: email
+            };
+            this.friends.push(newFriendContact);
         }
     }
 }
@@ -82,7 +92,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -114,5 +125,19 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
