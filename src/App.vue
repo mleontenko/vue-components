1bcb0ea87d15602c0de42c5dbab3ac7e610f5f34
Marin Leontenko <marin.leontenko@gmail.com>
2021-11-02 10:30:21 +0100

    using props

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,13 @@
                 name = "Manuel Lorenz"
                 phone-number="01234 78992"
                 email-address="manuel@localhost.com"
+                is-favorite="1"
             ></friend-contact>
             <friend-contact
                 name = "Julie Jones"
                 phone-number="11234 78992"
                 email-address="julie@localhost.com"
+                is-favorite="0"
             ></friend-contact>
         </ul>
     </section>
--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,11 +1,12 @@
 <template>
     <li>
-        <h2>{{ name }}</h2>
+        <h2>{{ name }} {{ friendIsFavorite === '1' ? '(Favorite)' : '' }}</h2>
+        <button @click="toggleFavorite">Toggle Favorite</button>
         <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }}</button>
         <ul v-if="detailsAreVisible">
             <li><strong>Phone:</strong> {{ phoneNumber }}</li>
             <li><strong>Email:</strong> {{ emailAddress }}</li>
-        </ul>
+        </ul>isFavorite
     </li>
 </template>
 
@@ -14,7 +15,8 @@ export default {
     props: [
         'name',
         'phoneNumber',
-        'emailAddress'
+        'emailAddress',
+        'isFavorite'
     ],
     data() {
         return {
@@ -25,12 +27,20 @@ export default {
                     phone: '0123 45678 90',
                     email: 'manuel@localhost.com'
                 },
+            friendIsFavorite: this.isFavorite,
         };
     },
     methods: {
         toggleDetails() {
             this.detailsAreVisible = !this.detailsAreVisible;
-        }
+        },
+        toggleFavorite() {
+            if (this.friendIsFavorite === "1") {
+                this.friendIsFavorite = "0";
+            } else {
+                this.friendIsFavorite = "1";
+            }
+        },
     }
 };
 </script>
